925c134093a73296f02bd11675b1ef9b4e2ceb05
Mia <mira.ikechukwu15@gmail.com>
2022-12-16 19:35:22 +0000

    Cold fix

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,9 +4,7 @@
 
 def uppercase(str):
     for i in range(len(str)):
-        """ could also be "for i in str """
         input = ord(str[i])
-        """If the above commented method is preferred then this code would be "input = ord(i)" """
         if input > 96 and input < 123:
             input -= 32
         print("{}".format(chr(input)), end="")
