5906584a1282603ca69f53a7092d2e6825d59a47
Mia <mira.ikechukwu15@gmail.com>
2022-12-16 18:25:44 +0000

    print combo, task 5

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@
 for tens in range(10):
     for units in range(tens + 1, 10):
         if tens == 8 and units == 9:
-            print("{}{} ".format(tens, units))
+            print("{}{}".format(tens, units))
         else:
             print("{}{}{} ".format(tens, units, chr(44)), end="")
