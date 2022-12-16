c2dd5d93bc955b9a5b32d92a3d76e72d6f77a864
Mia <mira.ikechukwu15@gmail.com>
2022-12-16 19:18:12 +0000

    to uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+# Write a function that prints a string in uppercase followed by a new line
+
+def uppercase(str):
+    for i in range(len(str)):
+# Can also be "for i in str"
+        input = ord(str[i])
+# If the above commented method is preferred then this code would be "input = ord(i)
+        if input > 96 and input < 123:
+            input -= 32
+        print("{}".format(chr(input)), end="")
+    print("")
