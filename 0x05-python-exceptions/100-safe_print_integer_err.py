18883691f50ddca4a787567df07f79f66f600686
Mia <mira.ikechukwu15@gmail.com>
2022-12-19 23:04:18 +0000

    Fixing check 5

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -2,12 +2,11 @@
 
 # A function that prints an integer
 
-import sys
-
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
     except (TypeError, ValueError) as error:
+        import sys
         print("Exception: {}".format(error), file=sys.stderr)
         return False
     else:
