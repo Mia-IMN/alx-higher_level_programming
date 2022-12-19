df2ab3584b91c89ebd7116ccac67ab2d911b754f
Mia <mira.ikechukwu15@gmail.com>
2022-12-19 23:20:50 +0000

    fixed new task

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,9 +4,8 @@
 
 def safe_function(fct, *args):
     try:
-        result = (fct(*args))
-        return result
-    except Exception as error: 
+        return(fct(*args))
+    except Exception as error:
         import sys
         print("Exception: {}".format(error), file = sys.stderr)
         return None
