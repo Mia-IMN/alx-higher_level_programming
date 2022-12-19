441579b4d1aa7412f3463461a8d11d32c9cb13dc
Mia <mira.ikechukwu15@gmail.com>
2022-12-19 09:43:50 +0000

    hot fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,7 +7,7 @@ import sys
 if __name__ == '__main__':
     if len(sys.argv) == 1:
         print("0 arguments.")
-    elif len(sys.argv) + 1 == 2:
+    elif len(sys.argv) == 2:
         print("{} argument:".format(len(sys.argv) - 1))
     else:
         print("{} arguments:".format(len(sys.argv) - 1))
