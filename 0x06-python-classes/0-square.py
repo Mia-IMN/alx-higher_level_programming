07bfa357f3240059ff217332d42c860582b95003
Mia <mira.ikechukwu15@gmail.com>
2022-12-20 19:20:42 +0000

    Fixed Documentation

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 
-"""python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
-
 class Square:
-    """This class Square is an empty class"""
+    """python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
     pass
