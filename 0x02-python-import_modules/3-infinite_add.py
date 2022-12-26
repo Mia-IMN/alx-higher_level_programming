bc601710c902209d4354667f8c5be50d05f820d4
Mia <mira.ikechukwu15@gmail.com>
2022-12-26 12:36:19 +0000

    python is cool

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+#A program that prints the result of the addition of all arguments
+
+import sys
+
+
+if __name__ == '__main__':
+    result = 0
+    for i in sys.argv[1:]:
+        result += int(i)
+    print("{:d}".format(result))
